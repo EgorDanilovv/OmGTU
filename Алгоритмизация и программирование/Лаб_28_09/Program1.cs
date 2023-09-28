becf7d5e9d2f852e2f98ab449a1e44d7e5d8cf9d
EgorDanilovv <e325361@gmail.com>
2023-09-28 21:51:56 +0600

    Create Program1.cs

---
+++ Алгоритмизация и программирование/Лаб_28_09/Program1.cs
@@ -0,0 +1,30 @@
+﻿class HelloWorld
+{
+    static void Main()
+    {
+        int n;
+        Console.WriteLine("Введите пожалуйста количество двальнейших символов");
+        n = Convert.ToInt32(Console.ReadLine());
+        int aktuelle_Element;
+        int vorheriges_Element = 0;
+        int swh = 0;
+        Console.WriteLine("Начинайте вводить последовательность");
+        for (int i = 0; i < n; i++)
+        {
+            aktuelle_Element = Convert.ToInt32(Console.ReadLine());
+            if (i == 0)
+            {
+                vorheriges_Element = aktuelle_Element;
+            }
+            else
+            {
+                if ((aktuelle_Element>=0 & vorheriges_Element<0)| (aktuelle_Element < 0 & vorheriges_Element >= 0))
+                {
+                    swh+=1;
+                }
+                vorheriges_Element = aktuelle_Element;
+            }
+        }
+        Console.WriteLine("Задание 1: "+swh);
+    }
+}
