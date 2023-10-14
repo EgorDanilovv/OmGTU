dc5ca49237db5453f4ee12c20cd566fc5de21222
Egor Danilov <98372907+EgorDanilovv@users.noreply.github.com>
2023-10-14 11:48:03 +0600

    Create mouse.cs

---
+++ Алгоритмизация и программирование/mouse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace OmGTU
+{
+    internal class First
+    {
+        static void Main(string[] args)
+        {
+            int x, bel, k, m, y;
+            k = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+            bel = Convert.ToInt32(Console.ReadLine());
+            x = 0;
+            int[] mouse = new int[k];
+            for (int i = 0; i < mouse.Length; i++) { mouse[i] = 1; }
+            while (mouse.Count(j => j == 1) > 1)
+            {
+                x += m;
+                Console.WriteLine(x);
+                if (mouse[(x % k)] == 1) { mouse[(x % k)] = 0; }
+                else
+                {
+                    while (mouse[(x % k)] == 0) { x++; }
+                    mouse[(x % k)] = 0;
+                    x += 1;
+                }
+            }
+            y = Array.IndexOf(mouse, 1);
+            Console.WriteLine((k + bel - y) % k);
+        }
+    }
+}
