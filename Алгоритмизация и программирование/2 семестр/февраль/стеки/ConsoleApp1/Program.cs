75a753cc98d64c191dbc691e5abc9ee1a3f82931
Egor Danilov <98372907+EgorDanilovv@users.noreply.github.com>
2024-02-28 13:49:57 +0600

    28.02

---
+++ Алгоритмизация и программирование/2 семестр/февраль/стеки/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static bool AreBracketsBalanced(string expression)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char ch in expression)
+        {
+            if (ch == '(' ||  ch == '{' || ch == '[')
+            {
+                stack.Push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']')
+            {
+                if (stack.Count == 0)
+                    return false;
+
+                char top = stack.Pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    static void Main()
+    {
+        string expression = Console.ReadLine();
+
+
+        if (AreBracketsBalanced(expression))
+        {
+            Console.WriteLine("Скобки в выражении сбалансированы.");
+        }
+        else
+        {
+            Console.WriteLine("Скобки в выражении не сбалансированы.");
+        }
+        Console.ReadKey();
+    }
+}
